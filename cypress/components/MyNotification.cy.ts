b36e0445cb38cde16d55c512ccc41f9c34aff943
onton <programm.rechner@gmail.com>
2025-03-14 01:19:16 +0400

    remove test

--- cypress/components/MyNotification.cy.ts
+++
@@ -1,8 +0,0 @@
-import MyNotification from '../../src/components/MyNotification.vue'
-
-describe('<MyNotification />', () => {
-  it('renders', () => {
-    // see: https://on.cypress.io/mounting-vue
-    cy.mount(MyNotification)
-  })
-})
